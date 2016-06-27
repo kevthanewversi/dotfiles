5715ffd660c89ed3c48ac28e9f7a2af5c29205f1
kevthanewversi <murimiclerk@gmail.com>
2016-06-27 14:37:33 +0300

    user now has the option to select installation of either zsh or bash as their shell

--- symlinks.sh
+++ symlinks.sh
@@ -10,41 +10,42 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim zshrc azureus codeblocks gnupg irssi java synaptic thumbnails vim"    # list of files/folders to symlink in homedir
 
-##########
+########## Functions
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
-
-echo "Would you like to install bash or zsh?"
-read -p  "1. bash  2. zsh  3. I'm good" shell
-
-
-
-
-case $shell in 
-1)
 
 install_bash (){
+	# Test to see if zshell is installed.  If it is:
+if [ -f /bin/bash -o -f /usr/bin/bash ]; then
+	echo "You already have bash installed"
+    # Clone my .bashrc repository from GitHub only if it isn't already present
+    if [[ ! -d $dir/.bashrc/ ]]; then
+        git clone http://github.com/kevthanewversi/dotfiles/bashrc.git
+    fi
+    # Set the default shell to bash if it isn't currently set to bash
+    if [[ ! $(echo $SHELL) == $(which bash) ]]; then
+        chsh -s $(which bash)
+    fi
+else
+    # If bash isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install bash and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        if [[ -f /etc/redhat-release ]]; then
+            sudo yum install bash
+            install_bash
+        fi
+        if [[ -f /etc/debian_version ]]; then
+            sudo apt-get install bash
+            install_bash
+        fi
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+        echo "Please install bash, then re-run this script!"
+        exit
+    fi
+fi
 
 }
-;;
-
-2)
 
 install_zsh () {
 # Test to see if zshell is installed.  If it is:
@@ -77,6 +78,41 @@ else
     fi
 fi
 }
+
+
+# create dotfiles_old in homedir
+# echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+# mkdir -p $olddir
+# echo "done"
+
+# # change to the dotfiles directory
+# echo -n "Changing to the $dir directory ..."
+# cd $dir
+# echo "done"
+
+# # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# for file in $files; do
+#     echo "Moving any existing dotfiles from ~ to $olddir"
+#     mv ~/.$file ~/dotfiles_old/
+#     echo "Creating symlink to $file in home directory."
+#     ln -s $dir/$file ~/.$file
+# done
+
+echo "Would you like to install bash or zsh?"
+read -p  "1. bash  2. zsh  3. I'm good   " shell
+
+
+
+
+case $shell in 
+1)
+
+install_bash
+;;
+
+2)
+
+install_zsh
 ;;
 
 3)
@@ -88,3 +124,7 @@ echo "Enter either 1,2 or 3"
 exit
 
 esac
+
+
+
+
